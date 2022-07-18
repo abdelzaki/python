5b3cd3b130829c80f173754ba30b9219b1862355
Your Name <you@example.com>
2022-07-18 18:26:05 +0200

    copy array using [:]

---
+++ code/leetcode/longestString.py
@@ -0,0 +1,25 @@
+"""Given a string s, find the length of the longest substring without repeating characters."""
+
+
+def lengthOfLongestSubstring(s: str):
+
+    uniqueSet = list()
+    uniqueSet = list()
+    length, maxlength = 0, 0
+    for char in s:
+        if char not in uniqueSet:
+            uniqueSet.append(char)
+            length += 1
+            maxlength = max(maxlength, length)
+        else:
+            uniqueSet.append(char)
+            index = uniqueSet.index(char)
+            uniqueSet = uniqueSet[index+1:]
+            length -= (index)
+            maxlength = max(maxlength, length)
+    return maxlength
+
+
+print("hi")
+
+print(lengthOfLongestSubstring(s))
