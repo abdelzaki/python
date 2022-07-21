875bb7392c2bd9ff4fbff60fb3247d072abb1a98
Your Name <you@example.com>
2022-07-21 12:39:45 +0200

    None as value

---
+++ code/leetcode/reversedLinkedList.py
@@ -0,0 +1,18 @@
+"""Given the head of a singly linked list, reverse the list, and return the reversed list."""
+
+from asyncio.windows_events import NULL
+
+import copy
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+            
+x = ListNode()
+x.val =12
+#y =ListNode()
+y = x
+y.val =2
+print(x)
