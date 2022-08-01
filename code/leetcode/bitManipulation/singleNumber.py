58bfbe062f0501fb4a8185026d890a335572016c
Your Name <you@example.com>
2022-08-01 13:50:41 +0200

    using xor

---
+++ code/leetcode/bitManipulation/singleNumber.py
@@ -0,0 +1,20 @@
+"""
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+"""
+
+
+class Solution:
+    def singleNumber(nums: list[int]) -> int:
+        uniqueset = set()
+        for num in nums:
+            if num not in uniqueset:
+                uniqueset.add(num)
+
+            else:
+                uniqueset.remove(num)
+        return uniqueset.pop()
+
+
+nums = [2, 2, 1]
+print(Solution.singleNumber(nums))
