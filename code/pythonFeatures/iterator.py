2bc586196723f26f58429d06b96f3d723a64dfd6
Your Name <you@example.com>
2022-08-15 08:40:48 +0200

    chainmapped

--- code/pythonFeatures/iterator.py
+++ code/pythonFeatures/iterator.py
@@ -41,8 +41,42 @@ def dictf():
     ypts = [10, 20, 30]
     xydict = dict(zip(xpts, ypts))
     xylist = list(zip(xpts, ypts))
+    print(xylist)
+    print(xydict)
+
     for k, v in xydict.items():
         print(k, " -> ", v)
 
 
-dictf()
+def getMinMaxfromDict():
+    """get the min and max according to the key """
+    mydict = {1: "a", 2: "b", 3: "c"}
+    print(min(mydict.keys()))
+    # according to the key
+    print(min(zip(mydict.keys(), mydict.values()))[1])
+
+
+def filterElements():
+    input = [1, 2, 3, "a", "b"]
+
+    def myfilterFunction(element):
+        try:
+            x = int(element)
+            return True
+        except:
+            return False
+
+    result = list(filter(myfilterFunction, input))
+    print(result)
+
+
+def chain():
+    from collections import ChainMap
+    fdict = {"1": 1, "2": 2}
+    sdict = {"3": 3, "2": 4}
+    mydict = ChainMap(fdict, sdict)
+    print(mydict)
+    print(mydict["2"])
+
+
+chain()
