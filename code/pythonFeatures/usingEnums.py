5c18fb1e97dcf6a2697cc8cbbd556db4123086a0
Your Name <you@example.com>
2022-08-18 06:35:03 +0200

    comment hinzufugen

--- code/pythonFeatures/usingEnums.py
+++ code/pythonFeatures/usingEnums.py
@@ -1,16 +1,19 @@
 """Enum it is used to make compare easier"""
 from enum import Enum, IntEnum
+
+# Enum does not support compare only == is supported.
 Size = Enum("Size", "s m l")
 mySize = Size.s
 yourSize = Size.m
 
 print(mySize == yourSize)
-anotherSize : Size  = Size.l
+anotherSize: Size = Size.l
 print(anotherSize)
 
+# it supports compare as it can be casted to integer
 IntSize = IntEnum("IntSize", "s m l")
 
 myIntSize = IntSize.s
 yourIntSize = IntSize.m
 
-print(myIntSize < yourIntSize )
+print(myIntSize < yourIntSize)
