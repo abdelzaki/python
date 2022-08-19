a90f7568f3a0641e530beafe419581415d95bfb3
Your Name <you@example.com>
2022-08-19 14:41:30 +0200

    t

---
+++ code/desiganPattern/stateMachine/stateMachine.py
@@ -0,0 +1,17 @@
+"""
+
+"""
+from state_machine import acts_as_state_machine, State, Event
+
+
+@acts_as_state_machine
+class Process:
+    created = State(initial=True)
+    waiting = State()
+    running = State()
+    terminated = State()
+    blocked = State()
+    swapped_out_waiting = State()
+    swapped_out_blocked = State()
+
+    wait = Event(from_state=)
